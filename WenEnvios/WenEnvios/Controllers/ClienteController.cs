230315c9ba21f62c2e221b2a65bf3d4151cd8844
andreiscar7 <andreiscar7@hotmail.com>
2023-08-24 23:48:38 -0500

    ENVIOS
    rastreo

---
+++ WenEnvios/WenEnvios/Controllers/ClienteController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WenEnvios.Models;
+
+
+namespace WenEnvios.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public EntityState EntityEstate { get; private set; }
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Cliente.ToList());
+            }
+                
+        }
+
+       
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int id)
+        {
+                using (DbModels context = new DbModels())
+                {
+                    return View(context.Cliente.Where(x => x.IDCliente == id));
+                }
+            
+         
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create(Cliente cliente)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    context.Cliente.Add(cliente);
+                    context.SaveChanges();
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Cliente.Where(x => x.IDCliente == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Cliente cliente)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DbModels context = new DbModels())
+                {
+                    context.Entry(cliente).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Cliente.Where(x => x.IDCliente == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    Cliente cliente = context.Cliente.Where(x => x.IDCliente == id).FirstOrDefault();
+                    context.Cliente.Remove(cliente);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WenEnvios/WenEnvios/Controllers/ProductoController.cs
@@ -0,0 +1,124 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using WenEnvios.Models;
+
+namespace WenEnvios.Controllers
+{
+    public class ProductoController : Controller
+    {
+       
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Producto.ToList());
+            }
+        }
+
+        // GET: Producto/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Producto.Where(x => x.IDProducto == id));
+            }
+
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create(Producto producto)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    context.Producto.Add(producto);
+                    context.SaveChanges();
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        // POST: Producto/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Producto.Where(x => x.IDProducto == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Producto/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Producto producto)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DbModels context = new DbModels())
+                {
+                    context.Entry(producto).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using(DbModels context = new DbModels())
+            {
+                return View(context.Producto.Where(x => x.IDProducto == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    Producto producto = context.Producto.Where(x => x.IDProducto == id).FirstOrDefault();
+                    context.Producto.Remove(producto);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
